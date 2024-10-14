f7bf0362f604408d5885a2480a60488fda2c9a8e
millie.davidson <millie.davidson@clear.bank>
2024-10-14 17:22:08 +0100

    Tidy up and added TODOs

--- millie_audio_project/Synthesizer.Designer.cs
+++ millie_audio_project/Synthesizer.Designer.cs
@@ -28,21 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            button2 = new Button();
+            groupBox1.SuspendLayout();
             SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(521, 264);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(969, 571);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(625, 215);
+            button1.Name = "button1";
+            button1.Size = new Size(188, 58);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(79, 228);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(197, 45);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(141, 492);
+            button2.Name = "button2";
+            button2.Size = new Size(188, 58);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
             // 
             // Synthesizer
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1729, 1017);
+            Controls.Add(button2);
+            Controls.Add(groupBox1);
             KeyPreview = true;
             Name = "Synthesizer";
             Text = "Synthesizer";
             Load += Synthesizer_Load;
             KeyDown += Synthesizer_KeyDown;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private CheckBox checkBox1;
+        private Button button1;
+        private Button button2;
     }
 }
--- millie_audio_project/Synthesizer.cs
+++ millie_audio_project/Synthesizer.cs
@@ -2,15 +2,15 @@ using System.Media;
 
 namespace millie_audio_project
 {
-    // This is a partial class so code can be added to the class without having to recreate the source file that
-    // includes automatically generated source
+    /* This is a partial class so code can be added to the class without having to recreate the source file that
+    includes automatically generated source */
     public partial class Synthesizer : Form
     {
         private const int SampleRate = 41000;
 
-        // Using short because it represents a 16-bit signed integer, which is needed for a sample rate of 16 bits
-        // A short can hold a max value of 32,767 and once this value is reached, it will decrement in value until 
-        // it reaches the minimum value of -32,768. Then it will go back up again.
+        /* Using short because it represents a 16-bit signed integer, which is needed for a sample rate of 16 bits.
+        A short can hold a max value of 32,767 and once this value is reached, it will decrement in value until
+        it reaches the minimum value of -32,768. Then it will go back up again */
         private const short BitsPerSample = 16;
 
         public Synthesizer()
@@ -22,47 +22,44 @@ namespace millie_audio_project
         {
         }
 
-        // Takes keyboard input (this is enabled via the Form: Properties > Misc > Key Preview > True)
+        //Takes keyboard input (this is enabled via the Form: Properties > Misc > Key Preview > True)
         private void Synthesizer_KeyDown(object sender, KeyEventArgs e)
         {
             short[] wave = new short[SampleRate];
-            // wave is an array of short values but binaryWriter only accepts byte values. Need to split each short
-            // into 2 bytes because a short is double the length of a byte. 1 BYTE = 8 BITS. 1 SHORT = 16 BITS
+            /* wave is an array of short values but binaryWriter only accepts byte values. Need to split each short
+            into 2 bytes because a short is double the length of a byte. 1 BYTE = 8 BITS. 1 SHORT = 16 BITS */
 
-            byte[] binaryWave = new byte[SampleRate * sizeof(short)]; // short because it's the data type of wave and
-            // we want 2 lots of 8 bits to create this byte array. We could just put 2 here but, using sizeof makes
-            // our intent clearer
+            byte[] binaryWave = new byte[SampleRate * sizeof(short)]; /* short because it's the data type of wave and
+            we want 2 lots of 8 bits to create this byte array. We could just put 2 here but, using sizeof makes
+            our intent clearer */
             
             float frequency = 440f;
 
-            // i is the unit of time for each sample in SampleRate
+            // i is the unit of time for each sample in SampleRate. TODO: Why 'i' for time? Isn't 't' or 'time' clearer?
             for (int i = 0; i < SampleRate; i++)
             {
-                // Algorithm from: https://www.youtube.com/watch?v=fp1Snqq9ovw
-                // This algorithm generates the sine wave (and is why the short values goes up and down like a WAVE)
-                // Could make this a method or function
+                /* Algorithm from: https://www.youtube.com/watch?v=fp1Snqq9ovw. This algorithm generates the sine wave
+                (and is why the short values go up and down, like a WAVE). TODO: Could make this a method or function */
                 wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin(((Math.PI * 2 * frequency) / SampleRate) * i));
             }
 
-            // Check the parameters required for the method below. Offset is 0, so it starts at the start. Splits every
-            // short into 2 bytes and writes them into binaryWave
-            // BlockCopy is resizing the data
+            /* Check the parameters required for the method below. Offset is 0, so it starts at the start. Splits every
+            short into 2 bytes and writes them into binaryWave. BlockCopy is resizing the data */
             Buffer.BlockCopy(wave, 0, binaryWave, 0, wave.Length * sizeof(short));
 
-            // To play the samples generated in the for loop, they have to be passed to software that will handle the
-            // digital to analogue conversion.The class below is a STREAM which will contain all the samples and some
-            // settings.This is because the sampler needs to know what sample rate to play the sample at etc. Wave file
-            // format config: http://soundfile.sapp.org/doc/WaveFormat/ - this is what we put into the stream
+            /* To play the samples generated in the for loop, they have to be passed to software that will handle the
+            digital to analogue conversion. The class below is a STREAM which will contain all the samples and some
+            settings. This is because the sampler needs to know what sample rate to play the sample at etc. Wave file
+            format config: http://soundfile.sapp.org/doc/WaveFormat/ - this is what we put into the stream */
 
-            // MemoryStream contains all the chunk information from the link above. Everything we write into MemoryStream
-            // with BinaryWriter comes from the requirements in the link above
-            // using statement is here to help with resource management. Frees up the connection with the file/object.
-            // Not having this can cause memory leaks
-            // Look up the code for Stream
-            using (MemoryStream memoryStream = new MemoryStream())
-            using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
+            /* MemoryStream contains all the chunk information from the link above. Everything we write into MemoryStream
+            with BinaryWriter comes from the requirements in the link above. using statement is here to help with resource
+            management - frees up the connection with the file/object (not having this can cause memory leaks). 
+            TODO: Look up the code for Stream */
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
             {
-                // The Wave file format specifies what these variables need 
+                // The Wave file format specifies what these variables need
                 short blockAlign = BitsPerSample / 8;
                 int subChunkTwoSize = SampleRate * blockAlign;
 
@@ -72,9 +69,9 @@ namespace millie_audio_project
                 binaryWriter.Write(new[] { 'W', 'A', 'V', 'E', 'f', 'm', 't', ' ' });
                 binaryWriter.Write(16); // For PCM, because that's all we're working with
 
-                // NOTE: the size in the requirements - 4 refers to RIFF (4 bytes, 1 byte per character), whereas 2 is
-                // 2 bytes because 16 bits = 2 bytes and a 16 bit integer = the SHORT data type. Which is why we have
-                // to cast 1 to a short below, so it only writes 2 bytes instead of 4
+                /* NOTE: the size in the requirements - 4 refers to RIFF (4 bytes, 1 byte per character), whereas 2 is
+                2 bytes because 16 bits = 2 bytes and a 16 bit integer = the SHORT data type. Which is why we have
+                to cast 1 to a short below, so it only writes 2 bytes, instead of 4 */
                 binaryWriter.Write((short)1);
                 binaryWriter.Write((short)1); // number of channels
                 binaryWriter.Write(SampleRate);
@@ -84,9 +81,8 @@ namespace millie_audio_project
                 binaryWriter.Write(new[] { 'd', 'a', 't', 'a' });
                 binaryWriter.Write(subChunkTwoSize);
 
-                // Now we have the stream of data, the SoundPlayer class can accept it
-                // memoryStream has a position (like a pointer) which is why we set the position below. 0 takes us to
-                // the start, to RIFF
+                /* Now we have the stream of data, the SoundPlayer class can accept it. memoryStream has a position
+                (like a pointer) which is why we set the position below. 0 takes us to the start (to RIFF) */
                 memoryStream.Position = 0;
                 new SoundPlayer(memoryStream).Play();
             }
